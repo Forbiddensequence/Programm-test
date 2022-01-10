7194e0b27fabf9db60a2b5009b701ed7467e6d6b
Denis Villeneuve <targaryen1337@yandex.ru>
2022-01-10 12:21:49 +0300

    final application

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+import com.company.APP.AppController;
+
+public class Main {
+
+    public static void main(String[] args) {
+        AppController app=new AppController();
+    }
+}
